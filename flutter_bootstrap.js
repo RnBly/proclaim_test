2382394f97102f1e43457894383bef3c23a1cae3
bomulkorea <bomulkorea@gmail.com>
2025-03-14 11:01:53 +0900

    copy_2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "800780835"
+    serviceWorkerVersion: "566821886"
   }
 });
